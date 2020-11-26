9f72d0a45f208bc4818cd147694df8c8514420cd
xinkumywi <48954067+xinkumywi@users.noreply.github.com>
2020-11-26 16:19:53 +0800

    Add files via upload
    a tic-tac-toe implement
    step 1 :
    compile all c file
    step 2 :
    link main.o & ga.o and main.o & gb.o respectively
    step 3 :
    run gb process followed by ga process

--- ga.c
+++ ga.c
@@ -4,14 +4,16 @@
 #include <stdio.h>
 #include "win.h"
 #include <tchar.h>
-
+#define my_turn 0
+#define chess 'X'
 TCHAR szName[]=TEXT("Global\\MyFileMappingObject");
-
+TCHAR szwait[]=TEXT("Y");
+TCHAR szMsg='a';
 #define BUF_SIZE 32
 int galoop(){
 	
    HANDLE hMapFile;
-   LPTSTR pBuf;
+   LPTSTR pBuf ;
 	int i = 0 ;
 	char c;
    hMapFile = CreateFileMapping(
@@ -42,10 +44,20 @@ int galoop(){
              GetLastError());
 
        CloseHandle(hMapFile);
+
       return 1;
    }
-	pBuf[7]=0;
-	pBuf[0]=0;
+   for(int i = 0 ;i < 32; i++)
+	pBuf[i] = 0;
+	
+	set_OK(pBuf);
+	while(1){
+	board(pBuf);
+	}
+	
+	sleep(30);
+	
+	/*
 while(1){
 	
 	if(pBuf[6] == 0){
@@ -65,12 +77,132 @@ while(1){
    else
 	   printf("\r wait data ........");
    sleep(1);
-}
+}*/
    UnmapViewOfFile(pBuf);
 
    CloseHandle(hMapFile);
 
-   return 0;
+   return 0;	
+}
+int set_OK(char * pBuf)
+{
+	printf("wait rival\n");
+	pBuf[0] = 1;
+	while(pBuf[1]!=1);
+	printf("rival is ok\n");
+}
+void board(char * pBuf)
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset], pBuf[board_offset+1],pBuf[board_offset+2]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset+3], pBuf[board_offset+4], pBuf[board_offset+5]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset+6], pBuf[board_offset+7], pBuf[board_offset+8]);
+
+    printf("     |     |     \n\n");
 	
+	if(pBuf[whosturn] == my_turn){
+		if(0){
+		INPUT_ERR:	
+		printf("vaild input\n");
+			
+		}
+		printf("your turn\n");
+		scanf("%d",&pBuf[30]);
+		switch(pBuf[30]){
+			case 1:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 2:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 3:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 4:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 5:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 6:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 7:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 8:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 0:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			default:
+			break;
+			
+		}
+		pBuf[whosturn]^=1;
+	}
+	else{
+		printf("enemy turn please wait\n");
+	}
 	
 }
--- gb.c
+++ gb.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
+#include "win.h"
 #pragma comment(lib, "user32.lib")
-#define BUF_SIZE 32
+#define BUF_SIZE 8
+#define my_turn 1
 TCHAR szName[]=TEXT("Global\\MyFileMappingObject");
-
+#define chess 'O'
 int galoop()
 {
    HANDLE hMapFile;
@@ -41,7 +43,13 @@ int galoop()
 
       return 1;
    }
-   
+  
+	set_OK(pBuf);
+	while(1){
+	board(pBuf);
+	}
+	sleep(30);
+  /*
 	while(1){
 	
    if(pBuf[6]== 1){
@@ -55,10 +63,135 @@ int galoop()
    printf("\r wait data ...");
    sleep(5);
 }
-
+*/
    UnmapViewOfFile(pBuf);
 
    CloseHandle(hMapFile);
 
    return 0;
+}
+
+int set_OK(char * pBuf)
+{
+	printf("wait rival\n");
+	pBuf[1] = 1;
+	while(pBuf[1]!=1);
+	printf("rival is ok\n");
+}
+void board(char * pBuf)
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset], pBuf[board_offset+1],pBuf[board_offset+2]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset+3], pBuf[board_offset+4], pBuf[board_offset+5]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", pBuf[board_offset+6], pBuf[board_offset+7], pBuf[board_offset+8]);
+
+    printf("     |     |     \n\n");
+	
+	if(pBuf[whosturn] == my_turn){
+		if(0){
+		INPUT_ERR:	
+		printf("vaild input\n");
+			
+		}
+		printf("your turn\n");
+		scanf("%d",&pBuf[30]);
+		switch(pBuf[30]){
+			case 1:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 2:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 3:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 4:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 5:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 6:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 7:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 8:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			case 0:
+			if(pBuf[board_offset+pBuf[30]] == 0){
+				pBuf[board_offset+pBuf[30]] = chess;
+			}
+			else{
+				goto INPUT_ERR;
+			}
+			break;
+			default:
+			break;
+			
+			
+		}
+		pBuf[whosturn]^=1;
+	}
+	else{
+		printf("enemy turn please wait\n");
+	}
+	
+	
 }
